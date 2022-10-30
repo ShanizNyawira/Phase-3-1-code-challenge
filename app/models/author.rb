d61b9d222d4f6fe6edf21aa30d9cf69030a02046
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-10-31 01:15:02 +0300

    Completed Assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,41 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
-  
   end
 
+  def name
+    @name
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    array = Article.all.select do |article|
+      article.author == self
+    end
+    array.map.uniq do |article|
+      article.magazine
+    end
+  end
+
+  def add_article(title, magazine)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    array = Article.all.select do |article|
+      article.author == self
+    end
+    array.map.uniq do |article|
+      article.magazine.category
+    end
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,11 +7,39 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author = {
+  name: "John Doe",
+  age: 30,
+  country: "USA"
+}
+
+magazine_object = {
+  name: "The New Yorker",
+  category: "News",
+  pages: 100
+}
 
 
 
+author = Author.new("Sarah J. Maas")
+author_1 = Author.new("J.R Ward")
+author_2 = Author.new("J.K Rowling")
+author_3 = Author.new("David Baldacci")
+author_4 = Author.new("James Patterson")
 
 
+magazine = Magazine.new("People Daily", "News")
+magazine_1 = Magazine.new("Daily Nation", "News")
+magazine_2 = Magazine.new("Vogue", "Fashion")
+magazine_3 = Magazine.new("The Standard", "News")
+magazine_4 = Magazine.new("Farmers Choice", "Farming")
+
+
+article = Article.new(author, magazine, "The New Yorker")
+article_1 = Article.new(author_1, magazine_1, "Daily Nation")
+article_2 = Article.new(author_2, magazine_2, "The Standard")
+article_3 = Article.new(author_3, magazine_3, "The New Yorker")
+article_4 = Article.new(author_4, magazine, "People Daily")
 
 
 
